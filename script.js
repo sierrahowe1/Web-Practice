cf425d8cc1e16f6522b920bcfa0f573a9967242b
sierrahowe1 <sierra.howe@unb.ca>
2026-01-10 17:32:16 -0400

    Update JS

--- script.js
+++ script.js
@@ -123,6 +123,7 @@ taskForm.addEventListener("submit", (e) => {
 
 clear.addEventListener("click", () => {
     tasks = tasks.filter((t) => !t.done);
+    console.log("Clear clicked");
     saveTasks();
     renderTasks();
 });
